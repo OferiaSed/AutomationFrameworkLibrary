377340612e5beff031ff27f89f8191b0b4d166e3
Omar Feria <OFeria@SedgwickCMS.com>
2021-10-07 10:00:51 -0500

    Adding Edge Driver

--- AutomationFramework/AutomationFramework/clsWebBrowser.cs
+++ AutomationFramework/AutomationFramework/clsWebBrowser.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Edge.SeleniumTools;
+﻿
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -37,17 +38,14 @@ namespace AutomationFramework
                     clsReportResult.fnLog("OpenBrowserPass", "Browser is openned correctly", "Pass", pblScreenShot);
                     break;
                 case "EDGE":
-                    /*
+                    var strDriverPath = fnGetProjectPath() + @"Debug\";
                     var optionsEdge = new EdgeOptions();
-                    optionsEdge.UseChromium = true;
-                    optionsEdge.BinaryLocation = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
-                    optionsEdge.AddArgument("no-sandbox");
-                    objDriver = new EdgeDriver(EdgeDriverService.CreateDefaultService(), optionsEdge, TimeSpan.FromMinutes(3));
+                    optionsEdge.AddAdditionalCapability("UseChromium", true);
+                    objDriver = new EdgeDriver(EdgeDriverService.CreateDefaultService(strDriverPath), optionsEdge, TimeSpan.FromMinutes(3));
                     objDriver.Manage().Timeouts().PageLoad.Add(System.TimeSpan.FromSeconds(10));
                     wait = new WebDriverWait(objDriver, TimeSpan.FromSeconds(10));
                     objDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                     objDriver.Manage().Window.Maximize();
-                    */
                     break;
                 default:
                     break;
@@ -67,6 +65,14 @@ namespace AutomationFramework
             clsReportResult.fnLog("NavigateURL", "Step - Navigated to the URL: " + pstrUrl, "Info", false);
             objDriver.Navigate().GoToUrl(pstrUrl);
             clsReportResult.fnLog("NavigateURLPass", "Navigated to the URL succesfully", "Pass", pblScreenShot);
+        }
+
+        private string fnGetProjectPath()
+        {
+            var strPath = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            var strActualPath = strPath.Substring(0, strPath.LastIndexOf("Debug"));
+            var strProjectPath = new Uri(strActualPath).LocalPath;
+            return strProjectPath;
         }
     }
 }
