a3ac3aa3255f21da8f577f1fcca618990cfea742
Omar Feria <OFeria@SedgwickCMS.com>
2021-12-02 16:03:09 -0600

    Path Config File update

--- AutomationFramework/AutomationFramework/clsVariables.cs
+++ AutomationFramework/AutomationFramework/clsVariables.cs
@@ -11,8 +11,32 @@ namespace AutomationFramework
         /// <summary>
         /// Constant to define the Automation Setting driver
         /// </summary>
-        public static string strGlobalConfigFile = @"H:\Any\4th_Automation\GlobalIntakeDriver\AutomationSettings.xlsx";
-        //public static string strGlobalConfigFile = @"C:\AutomationProjects\AutomationSettings.xlsx";
+        public static string strGlobalConfigFile
+        {
+            get
+            {
+                string blLocalExecution = null;
+                try
+                {
+                    blLocalExecution = Environment.GetEnvironmentVariable("GI_Env_Variable");
+                }
+                catch (NullReferenceException)
+                {
+                    //Ignore and continue
+                }
+                string path;
+                switch (blLocalExecution)
+                {
+                    case "LOCAL":
+                        path = @"C:\AutomationProjects\AutomationSettings.xlsx";
+                        break;
+                    default:
+                        path = @"H:\Any\4th_Automation\GlobalIntakeDriver\AutomationSettings.xlsx";
+                        break;
+                }
+                return path;
+            }
+        }
 
         /// <summary>
         /// Constant static for Edge Driver
